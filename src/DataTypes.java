94e46ac002f77010c007a73e655117359b222c6c
04kash <m04kashish@gmail.com>
2023-09-18 17:51:19 -0400

    Changed DataTypes.java

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTypes {
+    public static void main(String[] args) {
+        List<Integer> lst = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            lst.add(i);
+        }
+        System.out.println(DataTypes.sum(lst));
+    }
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0; //TODO: Check difference between long and int
         // below is a "foreach" loop which iterates through numbers
-        for (int x : numbers) {
+        for (long x : numbers) {
             s += x;
         }
         return s;
